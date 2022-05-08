60aeaceab22ea35288c4c76446bfcb5760b63166
zxk175 <zxk175@gmail.com>
2022-05-09 01:13:33 +0800

    11

--- openresty/standalone/build.sh
+++ openresty/standalone/build.sh
@@ -2,5 +2,5 @@ echo "================> 查看构建配置"
 docker-compose -f build.yml config
 
 echo "\n================> 开始构建镜像"
-docker-compose -f build.yml build --force-rm --no-rm --no-cache
-# docker-compose -f build.yml build --force-rm --no-rm
+# docker-compose -f build.yml build --force-rm --no-rm --no-cache
+docker-compose -f build.yml build --force-rm --no-rm
