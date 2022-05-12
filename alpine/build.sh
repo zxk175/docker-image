780f7445a61557c65cd1b5fb2a98af834ccb1a68
zxk175 <zxk175@gmail.com>
2022-05-13 06:37:58 +0800

    11

--- alpine/build.sh
+++ alpine/build.sh
@@ -2,4 +2,4 @@ echo "================> 查看构建配置"
 docker-compose -f build.yml config
 
 echo "\n================> 开始构建镜像"
-docker-compose -f build.yml build --force-rm --no-rm
+docker-compose -f build.yml build
