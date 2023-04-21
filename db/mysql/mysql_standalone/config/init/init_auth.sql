43fa4c1e7a4bcc9f730e47b7e63f759913d42ea1
zxk175 <zxk175@gmail.com>
2023-04-21 23:39:41 +0800

    add nacos

--- db/mysql/mysql_standalone/config/init/init_auth.sql
+++ db/mysql/mysql_standalone/config/init/init_auth.sql
@@ -2,9 +2,7 @@ USE mysql;
 
 -- 授权远程连接
 GRANT ALL PRIVILEGES ON *.*  TO 'root'@'%';
-
 -- 修改root密码
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Zxk123456';
-
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 -- 刷新权限
 FLUSH PRIVILEGES;
---
+++ nacos/nacos_standalone/start.sh
@@ -0,0 +1,5 @@
+# 删除容器
+docker-compose -f ./docker-compose.yml down
+
+# 启动redis集群
+docker-compose -f ./docker-compose.yml up -d
