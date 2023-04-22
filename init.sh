5af73c29a6ae700bfb953084b3d7f810d555cf14
zxk175 <zxk175@gmail.com>
2023-04-22 15:16:04 +0800

    add seata

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 # 创建docker网络
 docker network rm well
-docker network create --driver bridge --subnet 192.168.0.0/16 --gateway 192.168.0.1 well
+docker network create --driver bridge --subnet 172.30.0.0/24 --gateway 172.30.0.1 well
 docker network ls
 
 # 查看网络详情
